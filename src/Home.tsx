5df61f6146ccf212952e94028fb59f91e5149b31
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-12-28 16:18:04 +0530

    Testing nativewind

---
+++ src/Home.tsx
@@ -0,0 +1,9 @@
+import { ScrollView, Text } from 'react-native';
+
+export default function Home() {
+  return (
+    <ScrollView>
+      <Text className="text-red-700">Testing tailwind</Text>
+    </ScrollView>
+  );
+}
