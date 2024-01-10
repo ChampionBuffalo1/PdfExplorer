48b76a5acad9fb0357acd7c78dfc3052b492f20a
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-01-11 01:16:50 +0530

    Rendering PDF (Simple)

---
+++ src/screens/PdfView.tsx
@@ -0,0 +1,36 @@
+import Pdf from 'react-native-pdf';
+import { useEffect, useState } from 'react';
+import type { StackScreenProps } from '../../NavigationTypes';
+import { Dimensions, View, Text, StyleSheet } from 'react-native';
+
+export default function PdfView({ route }: StackScreenProps<'PdfView'>) {
+  const [uri, setUri] = useState<string>('');
+
+  useEffect(() => {
+    if (route.params?.uri) {
+      setUri(route.params.uri);
+    }
+  }, [route, setUri]);
+
+  return (
+    <View className="flex-1 items-center justify-start">
+      {uri && (
+        <Pdf
+          style={style.pdf}
+          source={{
+            uri,
+          }}
+        />
+      )}
+      {!uri && <Text className="text-black">No URI Found</Text>}
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  pdf: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
