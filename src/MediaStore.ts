c4a20ed69210c1f5917a06c23395110d2089c1bf
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-01-14 01:32:58 +0530

    Added types

--- src/MediaStore.ts
+++ src/MediaStore.ts
@@ -3,7 +3,8 @@ import { NativeModules } from 'react-native';
 const { MediaStore } = NativeModules;
 
 interface MediaStoreInterface {
-  getThumbnail(uri: string, callback: (thumbnail: string) => unknown): void;
+  getThumbnail(path: string): Promise<string>;
+  getThumbnailWithOptions(path: string, quality: number, width: number, height: number): Promise<string>;
 }
 
 export default MediaStore as MediaStoreInterface;
